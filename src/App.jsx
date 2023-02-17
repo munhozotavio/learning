6223ee3ff9a169624387d5cfd931e97a9fd271c6
munhozotavio <otaviosilmunhoz@gmail.com>
2023-02-17 19:57:05 -0300

    added fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import './App.css'
 
 function App() {
   const [word, setWord] = useState('');
+  const [synonyms, setSynonyms] = useState([]);
 
   const handleWordChange = e => {
     setWord(e.target.value);
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    // get only results with more than 1000 points;
+    fetch(`https://api.datamuse.com/words?rel_syn=${word}`).then(resp => resp.json()).then(resp => setSynonyms(resp.filter(synonym => synonym.score > 1000)));
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <label for="wordInput">Enter your word</label>
         <input id="wordInput" type="text" onChange={(e) => handleWordChange(e)}/>
         <input type="submit" value="GO"/>
