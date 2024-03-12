f457a57e194e60d7ae4dae27362f37cd72ec3e20
munhozotavio <otaviosilmunhoz@gmail.com>
2024-03-12 19:01:17 -0300

    first class
    Cleaned the repo and added a hello world file

---
+++ hello_world.js
@@ -0,0 +1,11 @@
+const pName = "Jonas!";
+console.log('Hello, ' + pName + '!');
+// --------------------------------- //
+class Product {
+    pName;
+    pValue;
+    constructor(pName, pValue) {
+        this.pName = pName;
+        this.pValue = pValue;
+    }
+}
---
+++ src/hello_world.ts
@@ -0,0 +1,13 @@
+const pName = "Jonas!";
+console.log('Hello, ' + pName + '!');
+// --------------------------------- //
+
+class Product{
+  pName: string;
+  pValue: number;
+
+  constructor(pName: string, pValue: number){
+    this.pName = pName;
+    this.pValue = pValue;
+  }
+}
