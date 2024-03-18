0956cc153cecf48c914d8be48d340e5163562487
munhozotavio <otaviosilmunhoz@gmail.com>
2024-03-18 14:30:47 -0300

    Create basic_types.ts

---
+++ src/basic_types.ts
@@ -0,0 +1,7 @@
+let weekDay: string;
+let year: number;
+let month: string | number; //This is a union type
+
+
+year = +"2024"; //This works because the + operator is used to convert a string to a number
+
