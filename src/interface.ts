f5042a418b1d43e82d5adb48d0e4fe1e32142149
munhozotavio <otaviosilmunhoz@gmail.com>
2024-04-02 18:08:45 -0300

    functions and interfaces

---
+++ src/interface.ts
@@ -0,0 +1,19 @@
+interface IAddress {
+  street: string;
+  address: string;
+  num: number;
+  city: string;
+  zip: string;
+}
+
+let address: IAddress
+
+address = {
+  street: "Rua das Flores",
+  address: "Casa",
+  num: 123,
+  city: "São Paulo",
+  zip: "12345-123"
+}
+
+
