2724eea6fad6b6fe2ec0dca8f36d1e741dc98763
munhozotavio <otaviosilmunhoz@gmail.com>
2024-11-16 11:33:24 -0300

    added first example

---
+++ learning-next/pages/basics/first_component.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+
+export default function fistComponent():(ReactElement | null) {
+  //return null
+  return <div> first<h1>First react component</h1></div>;
+}
